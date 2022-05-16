6710bbab9ed8d289e7732095d2e8f5009de844b0
ZAZAZAZAZAZAZAZAZAZA <ralfs.putnins@marupe.edu.lv>
2022-05-16 16:02:59 +0300

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -1,28 +1,19 @@
-<!DOCTYPE html>
-chtml lang="en">
-chead>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.8">
-<title>Dienasgrāmata</title>
-</head>
-<body>
-ahp
+<?php
+ini_set('display_errors', 1);
+error_reporting(-1);
 include("db.php");
-$sql = "SELECT FROM users";
-$result = Sconn->query ($sq1);
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
 if ($result->num_rows > 0) {
-while(Srow = Sresult->fetch_assoc ()) {
-%3D
-Sid - Srow['id'];
-$name = Srow['name'];
-$sname = $row['$name'];
-$login - Srow['login'];
-Spass - Srow[ 'password'];
-echo "$id"." "."$name"." "."$sname".
-"$sname"." "."$login"." ".$pass."cbr>";
-}
-}
-Sconn->close();
-7>
-</body>
-ghtm15
+    while($row = $result->fetch_assoc()) {
+                $id = $row['id'];
+                $name = $row['name'];
+                $sname = $row['sname'];
+                $login = $row['login'];
+                $pass = $row['password'];
+            echo "$id" . " " . "$name" . " " . "$sname" . " " . "$login" . " " . "$date_c" . " .$pass. "<br>";
+        }
+    }
+
+$conn->close();
+?>
